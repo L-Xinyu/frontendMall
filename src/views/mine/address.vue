ebdf31e0c6ec38a47c11c260ab2e339165b9f77e
XinyuLi <911410598@qq.com>
2020-06-27 17:07:46 +0200

    Add method save user address and refactor code

--- src/views/mine/address.vue
+++ src/views/mine/address.vue
@@ -26,14 +26,14 @@
                             <div class="form-group">
                                 <label for="name" class="col-sm-2 control-label">Name：</label>
                                 <div class="col-sm-6">
-                                    <Input id="name"
+                                    <Input v-model="name" id="name"
                                            placeholder="Please enter your name..." style="width: 300px"/>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="details" class="col-sm-2 control-label">Address：</label>
                                 <div class="col-sm-10">
-                                    <input class="form-control" id="details"
+                                    <input v-model="consignee_info" class="form-control" id="details"
                                            placeholder="Please fill in the detailed delivery address,such as street name, house number, etc."
                                            maxlength="200" type="text">
                                 </div>
@@ -41,13 +41,13 @@
                             <div class="form-group">
                                 <label for="mobile" class="col-sm-2 control-label">Phone：</label>
                                 <div class="col-sm-6">
-                                    <input  class="form-control" id="mobile" placeholder="Please enter phone number" type="text">
+                                    <input v-model="phone_number" class="form-control" id="mobile" placeholder="Please enter phone number" type="text">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="name" class="col-sm-2 control-label">As Default：</label>
                                 <div class="user-form-group col-sm-6" >
-                                    <RadioGroup>
+                                    <RadioGroup v-model="is_default">
                                         <Radio label="1">
                                             <span>Yes</span>
                                         </Radio>
@@ -59,7 +59,7 @@
                             </div>
                             <div class="form-group">
                                 <div class="col-sm-offset-2 col-sm-6">
-                                    <button type="submit" class="but">Save</button>
+                                    <button type="submit" class="but" @click="save()">Save</button>
                                 </div>
                             </div>
                         </form>
@@ -75,20 +75,44 @@
     import header_ from '../../components/header_'
     import footer_ from '../../components/footer_'
     import mineLeft from '../../components/mine_left'
+    import {addAddress} from "../../lib/interface";
 
     export default {
         components: {header_, footer_, mineLeft},
         name: "mine-address",
         data() {
             return {
-                name: ""
+                name: "",
+                phone_number: "",
+                consignee_info: "",
+                is_default: "0",
             }
         },
         mounted() {
             $("#address").addClass("active");
         },
         methods: {
-
+            async save() {
+                if (this.name === "") {
+                    this.$Message.error('Please enter your name！');
+                    return;
+                }
+                if (this.phone_number === "") {
+                    this.$Message.error('Please enter your mobile！');
+                    return;
+                }
+                if (this.consignee_info === "") {
+                    this.$Message.error('Please enter your address！');
+                    return;
+                }
+                let result = await addAddress({
+                    "name": this.name,
+                    "phone_number": this.phone_number,
+                    "consignee_info": this.consignee_info,
+                    "is_default": this.is_default,
+                });
+                this.$Message.success(result.message);
+            }
         }
     }
 </script>
