e3be45c3c7a525c235a1414cffadc06dc0d2cfb1
XinyuLi <911410598@qq.com>
2020-06-30 14:36:41 +0200

    Fixed bug and Specification API interface

--- src/components/search.vue
+++ src/components/search.vue
@@ -4,20 +4,6 @@
             <router-link class="logo" to="/">
                 <img src="@/assets/images/icons/logo.png" alt="TP6" class="cover">
             </router-link>
-            <div class="search-box">
-                <form class="input-group">
-                    <input placeholder="Search For..." v-model="CurText" type="text">
-                    <span class="input-group-btn">
-						<button type="button">
-							<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-						</button>
-					</span>
-                </form>
-                <p class="help-block text-nowrap">
-                    <router-link style="margin-left: 5px;"
-                                 :to="'/search?text='"></router-link>
-                </p>
-            </div>
             <div class="cart-box">
                 <router-link to="/cart" class="cart-but"><i class="iconfont icon-shopcart cr fz16"></i> Shopping Cart <span
                         v-text="cartCount"></span>
--- src/lib/interface.js
+++ src/lib/interface.js
@@ -9,8 +9,6 @@ const url = process.env.NODE_ENV === 'production'
     ? ''
     : '/api/';  //localhost
 
-
-
 //Send SmsCode
 export const smscode = (data) => post(url + "api/auth/smscode", data);
 //Login
@@ -19,24 +17,28 @@ export const login = (data) => post(url + "api/auth/login", data);
 export const reg = (data) => post(url + "api/auth/register", data);
 //Logout api
 export const logout = (data) => post(url + "api/user/logout", data);
+
 //Count ShoppingCart api
 export const cartCount = (data) => post(url + "api/mall/init", data);
-//Index category
-export const category = (data) => get(url + "api/category", data);
-//Index banner
-export const banner = (data) => get(url + "api/index/getRotationChart", data);
-//Index recommend
-export const goodsRecommend = (data) => get(url + "api/index/cagegoryGoodsRecommend", data);
-//Category classification list
-export const categorySearch = (data) => get(url + "api/category/search/" + data.id);
-//Get category sku
-export const sku = (data) => get(url + "api/sku", data);
 //Category and goods list
 export const lists = (data) => get(url + "api/mall/lists", data);
 //Good Detail
 export const detail = (data) => get(url + "api/mall/detail/" + data.id);
 //New Goods Recommend
 export const recommend = (data) => get(url + "api/mall/recommend",data);
+
+//Index category
+export const category = (data) => get(url + "api/category", data);
+//Category classification list
+export const categorySearch = (data) => get(url + "api/category/search/" + data.id);
+//Get category sku
+export const sku = (data) => get(url + "api/sku", data);
+
+//Index banner
+export const banner = (data) => get(url + "api/index/getRotationChart", data);
+//Index recommend
+export const goodsRecommend = (data) => get(url + "api/index/cagegoryGoodsRecommend", data);
+
 //Add Shopping Cart
 export const addCart = (data) => post(url + "api/cart/add", data);
 //ShoppingCart list
@@ -45,16 +47,19 @@ export const cartList = (data) => get(url + "api/cart/lists", data);
 export const deleteCart = (data) => post(url + "api/cart/delete", data);
 //Update cart data
 export const updateCart = (data) => post(url + "api/cart/update", data);
+
 //Add User Address
 export const addAddress = (data) => post(url + "api/address/add", data);
 //Address
 export const address = (data) => get(url + "api/address/read", data);
+
 //Post Order
 export const order = (data) => post(url + "api/order", data);
 //Get Order inform
 export const orderInfo = (data) => get(url + "api/order/" + data.id);
 //Ger user all Orders
 export const orderList = (data) => get(url + "api/order/all", data);
+
 //Get User Information
 export const user = (data) => get(url + "api/user", data);
 //Update User
